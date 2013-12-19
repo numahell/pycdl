e228d47f57f328a09efa71a15fac2653808cd8b1
Emmanuelle Helly <emmanuelle.helly@makina-corpus.com>
2013-12-20 00:22:12 +0100

    Add conference listing generation

--- pycdl_schedule_genrst.py
+++ pycdl_schedule_genrst.py
@@ -12,68 +12,135 @@ def get_time_from_epoch(epoch):
 
 
 days = ['23','24']
-scheduleurl = "http://localhost:8000/files/francejs-schedule.json"
-donnee = JizonSchedule(scheduleurl, days)
-print "retrieving information ..."
-donnee.retrieveData()
-BASEDIR = "/home/numahell/Dev/pelican/capitoledulibre-site/src/pages/schedule"
-VIDEO_URLBASE = "http://toulibre.org/pub/2013-11-23-capitole-du-libre/videos"
+scheduleurl_francejs = "http://localhost:8000/files/francejs-schedule.json"
+scheduleurl_cdl = "http://localhost:8000/files/capitole-du-libre-schedule.json"
+BASEDIR = "/home/numahell/Dev/pelican/capitoledulibre-site/src/pages/videos-conferences"
+VIDEO_URLBASE = "http://stream.toulibre.org/cdl2013"
 THEMES = {
     "A001": u"grand-public",
     "A002": u"multimedia-bureautique",
-    "A201": u"Technique",
-    "A202": u"france-js",
+    "A201": u"technique",
+    "A202": u"francejs",
     "A203": u"multimedia-bureautique",
     "C002": u"internet-libre",
     "C103": u"akademy-fr",
 }
 
-try:
-    os.mkdir(BASEDIR)
-except:
-    pass
-os.chdir(BASEDIR)
+def gen_rst_pages(scheduleurl):
 
-conferences = donnee.getConferences()
+    donnee = JizonSchedule(scheduleurl, days)
+    print "retrieving information ..."
+    donnee.retrieveData()
+
+    try:
+        os.mkdir(BASEDIR)
+    except:
+        pass
+    os.chdir(BASEDIR)
+
+    conferences = donnee.getConferences()
+
+    for w in conferences:
+        
+        title = w['title']
+        rstheading = u"="*len(title)
+        start = get_time_from_epoch(w['start_time_epoch'])
+        end = get_time_from_epoch(w['end_time_epoch'])
+        wid = putils.slugify(w['title'])
+        space = w['space']
+        theme = THEMES[w['space']]
+        theme = "francejs"
+        abstract = w['abstract']
+        speakers = ""
+        if w.has_key('speakers'):
+            speakers_names = [s['name'] for s in w['speakers']]
+            speakers = ', '.join(speakers_names)
+        
+        file_name = "%s.rst" % wid
+        
+        output = '\n'.join([rstheading, 
+                            title, 
+                            rstheading, 
+                            u"",
+                            u""":url: conferences/%s/%s.html""" % (theme, wid),
+                            u""":save_as: conferences/%s/%s.html""" % (theme, wid),
+                            u""":video_url: %s/%s/%s""" % (VIDEO_URLBASE, theme, wid),
+                            u""":speakers: %s""" % speakers,
+                            u":template: conference",
+                            u"",
+                            u""".. html::""",
+                            u"",
+                            u" %s" % abstract.replace('\n',''),
+                            u"",
+                            u"",
+                            ])
+
+        f = open(file_name, 'w')
+        f.write(output.encode('utf8'))
+        f.close()
+
+        print '%s file written' % file_name
 
 
-for w in conferences:
+def gen_rst_lists(scheduleurl):
+
+    donnee = JizonSchedule(scheduleurl, days)
+    print "retrieving information ..."
+    donnee.retrieveData()
+
+    conferences = donnee.getConferences()
     
-    title = w['title']
-    rstheading = u"="*len(title)
-    start = get_time_from_epoch(w['start_time_epoch'])
-    end = get_time_from_epoch(w['end_time_epoch'])
-    wid = putils.slugify(w['title'])
-    space = w['space']
-    #~ theme = THEMES[w['space']]
-    theme = "france-js"
-    abstract = w['abstract']
-    speakers = ""
-    if w.has_key('speakers'):
-        speakers_names = [s['name'] for s in w['speakers']]
-        speakers = ', '.join(speakers_names)
+    conferences_list = {}
+    output = ""
+    themes = THEMES
+    for theme in themes.values():
+        conferences_list[theme] = u""
     
-    file_name = "%s.rst" % wid
+    for w in conferences:
+        
+        title = w['title']
+        wid = putils.slugify(w['title'])
+        theme = THEMES[w['space']]
+        speakers = ""
+        if w.has_key('speakers'):
+            speakers_names = [s['name'] for s in w['speakers']]
+            speakers = ', '.join(speakers_names)
+        
+        conferences_list[theme] += u"""* `%s </conferences/%s/%s.html>`_ - par %s\n""" % (title, theme, wid, speakers)
+
+    for theme in themes.values():
+        
+        rstheading = u"="*len(theme)
+        output = "\n".join([rstheading,
+                            theme,
+                            rstheading,
+                            u"",
+                            u":url: conferences/%s/" % theme,
+                            u":save_as: conferences/%s/index.html" % theme,
+                            u":template: conferences"
+                            u"",
+                            u"",
+                            conferences_list[theme],
+                            ])
+        theme_basedir = '/'.join([BASEDIR, theme])
+        
+        try:
+            os.mkdir(theme_basedir)
+        except:
+            pass
+        os.chdir(theme_basedir)
     
-    output = '\n'.join([rstheading, 
-                        title, 
-                        rstheading, 
-                        u"",
-                        u""":url: conferences/%s/%s.html""" % (theme, wid),
-                        u""":save_as: conferences/%s/%s.html""" % (theme, wid),
-                        u""":video_url: %s/%s""" % (VIDEO_URLBASE, theme),
-                        u""":speakers: %s""" % speakers,
-                        u":template: conference",
-                        u"",
-                        u""".. html::""",
-                        u"",
-                        u" %s" % abstract.replace('\n',''),
-                        u"",
-                        u"",
-                        ])
+        file_name = "index.rst"
+        f = open(file_name, 'w')
+        f.write(output.encode('utf8'))
+        f.close()
 
-    f = open(file_name, 'w')
-    f.write(output.encode('utf8'))
-    f.close()
+        print '%s file written' % file_name
 
-    print '%s file written' % file_name
+def main():
+	
+	#~ gen_rst_lists(scheduleurl_cdl)
+
+if __name__ == '__main__':
+	main()
+
